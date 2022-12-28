f1bf6caff95d8247fc268e240791a03676121975
kimjunwon12 <121483268+kimjunwon12@users.noreply.github.com>
2022-12-28 19:30:12 +0900

    28일 제이커리 연습

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,16 @@
 function inform(){
     alert("나는 alert 입니다.");
 }
+
+function grade(){
+
+
+}
+document.getE1ementByid('total').style.fontsize = '30px'
+$(#total),css({
+    fontsize:30
+});
+
+var kor1 = parseInt(document.getE1ementByid("kor1").innetHTML);
+var kor2 = parseInt(document.getE1ementByid("kor2").innetHTML);
+var first  = document.getE1ementByid
